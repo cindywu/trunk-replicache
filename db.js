087601dd4aa38b935e145cce5804c560869e0850
cindy <ciindy.wu@gmail.com>
2021-05-11 17:01:34 -1000

    initiate db one time

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 import pgInit from 'pg-promise'
 
+const pgp = pgInit()
+const db = pgp({
+  connectionString: process.env.REPLICHAT_DB_CONNECTION_STRING,
+})
+
 export async function getDB() {
-  const pgp = pgInit()
-  const db = pgp({
-    connectionString: process.env.REPLICHAT_DB_CONNECTION_STRING,
-  })
   await db.connect()
   return db
 }
+
